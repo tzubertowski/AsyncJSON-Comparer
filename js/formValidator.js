624a5b58cf767552f0186a66570cf7fbc398780a
Tomasz Żubertowski <tzubertowski@sigma.ug.edu.pl>
2015-05-21 23:41:39 +0200

    Simple form validation

--- js/formValidator.js
+++ js/formValidator.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
-  alert('install gentoo');
+  var urlFormField = $('input[name=jsonUrl]');
+  var refreshFormField = $('input[name=jsonRefresh]');
+  var configForm   = $('#configFormMain');
+  var contentWrapper = $('#content-wrapper');
 
+  $('#toggleAjax').click(function(e){
+      var urlText = urlFormField.val();
+      var refreshVal = refreshFormField.val();
+
+      if(/^([a-z]([a-z]|\d|\+|-|\.)*):(\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?((\[(|(v[\da-f]{1,}\.(([a-z]|\d|-|\.|_|~)|[!\$&'\(\)\*\+,;=]|:)+))\])|((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=])*)(:\d*)?)(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*|(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)|((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)|((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)){0})(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(urlText)) {
+        $(this).attr('Checked','Checked');
+      } else {
+          renderAlert(contentWrapper, 'danger', 'Error', 'Bad URL or no URL given. Please fix that.', true);
+          return false;
+      }
+
+      if(refreshVal == '') {
+        refreshFormField.val('60');
+      }
+
+
+      return;
+  });
+
+
+  var renderAlert = function(target, type, header, message, hide) {
+      target.prepend('<div class="alert alert-'+type+'" id="app-alert"><button type="button" class="close" data-dismiss="alert">x</button><strong>'+header+'! : </strong>'+message+'</div>');
+      if( hide == true) {
+        setTimeout(function(){
+          $('#app-alert').fadeOut('slow', function(){$(this).remove});
+        }, 3500);
+      }
+  }
 });
